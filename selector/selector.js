7fcb3bf3bbbe9346fa97006b9dee3c0e4d3f779e
JingxiGao <jingxi.gao@tum.de>
2018-10-19 17:43:26 +0200

    JavaScript

---
+++ selector/selector.js
@@ -0,0 +1,15 @@
+// $(document).ready(function () {
+//     //alert("文档加载完毕！");
+//     $("p").click(function () {
+//         $(this).hide();
+//     });
+// });
+
+//id class 标签选择器
+$(document).ready(function () {
+    $("button").click(function () {
+        //$("#p1").text("p1元素被修改了")
+        $(".p3").text("p3元素被修改了");
+    });
+
+});
