ce82315717aa9c8e8b163e734bd2a4fddd70c87b
JingxiGao <jingxi.gao@tum.de>
2018-10-19 17:58:02 +0200

    jQ事件方法

---
+++ event/eventMethod.js
@@ -0,0 +1,18 @@
+// $(document).ready(function () {
+//     $("button").click(function () {
+//        $(this).hide();//单机隐藏按钮
+//     });
+// });
+
+
+// $(document).ready(function () {
+//     $("button").dblclick(function () {
+//         $(this).hide();//双击隐藏按钮
+//     });
+// });
+
+$(document).ready(function () {
+    $("button").mouseenter(function () {
+        $(this).hide();//鼠标移动上
+    });
+});
